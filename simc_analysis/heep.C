515f1de75c80e2ca540913d07269daa858acfcfc
Carlos Yero <cyero002@fiu.edu>
2018-01-31 22:33:10 -0500

    added option for spec arm in heep.C

--- simc_analysis/heep.C
+++ simc_analysis/heep.C
@@ -1,4 +1,4 @@
-//Analysis for COIN. runs 1854 AND 1866, shms->electrons, hms->protons
+//Analysis for HMS/SHMS coincidence H(e,e'p)
 
 
 #define heep_cxx
@@ -15,7 +15,7 @@ void heep::Loop()
 //      root> t.GetEntry(12); // Fill t data members with entry number 12
 //      root> t.Show();       // Show values of entry 12
 //      root> t.Show(16);     // Read and show values of entry 16
-//      root> t.Loop();       // Loop on all entries
+//      root> t.Loop("HMS", "SHMS");       // Loop on all entries
 //
 
 //     This is the loop skeleton where:
@@ -34,12 +34,23 @@ void heep::Loop()
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
 
+   TString electron_arm;
+   TString hadron_arm;
+   cout << "Enter electron arm (SHMS or HMS): " << endl;
+   cin >> electron_arm;
+
+   if (electron_arm=="SHMS")
+     {
+       hadron_arm ="HMS";
+     }
+   else{hadron_arm = "SHMS";}
+   
    string simc_file;   //added
    TString f0 = "../worksim/simc_ROOTfiles_list.data";
    ifstream  infile(f0);
    infile >> simc_file;
-   
-   
+
+  
     // define histograms
    Double_t pi = 3.141592654;
    Double_t dtr = pi/180.;
@@ -73,29 +84,29 @@ void heep::Loop()
    TH1F *z_tar = new TH1F("z_tar", "z_Target", bins, -5.5, 5.5);
    
    //Hadron arm Reconstructed Quantities ( xtar, ytar, xptar, yptar, delta)
-   TH1F *hytar = new TH1F("hytar", "SHMS Y_{tar}", bins, -4., 4.);
-   TH1F *hxptar = new TH1F("hxptar", "SHMS X'_{tar}", bins, -0.08, 0.08 );
-   TH1F *hyptar = new TH1F("hyptar", "SHMS Y'_{tar}", bins, -0.04, 0.04 );
-   TH1F *hdelta = new TH1F("hdelta", "SHMS Momentum Acceptance, #delta", bins, -10., 10. );
+   TH1F *hytar = new TH1F("hytar", hadron_arm + " Y_{tar}", bins, -4., 4.);
+   TH1F *hxptar = new TH1F("hxptar", hadron_arm + " X'_{tar}", bins, -0.08, 0.08 );
+   TH1F *hyptar = new TH1F("hyptar", hadron_arm + " Y'_{tar}", bins, -0.04, 0.04 );
+   TH1F *hdelta = new TH1F("hdelta", hadron_arm + " Momentum Acceptance, #delta", bins, -10., 10. );
 
    //Hadron arm Focal Plane Quantities
-   TH1F *hxfp = new TH1F("hxfp", "SHMS X_{fp}", bins, -30., 20.);
-   TH1F *hyfp = new TH1F("hyfp", "SHMS Y_{fp}", bins, -15., 10.);
-   TH1F *hxpfp = new TH1F("hxpfp", "SHMS X'_{fp}", bins, -0.06, 0.06 );
-   TH1F *hypfp = new TH1F("hypfp", "SHMS Y'_{fp}", bins, -0.03, 0.02);
+   TH1F *hxfp = new TH1F("hxfp", hadron_arm + " X_{fp}", bins, -30., 20.);
+   TH1F *hyfp = new TH1F("hyfp", hadron_arm + " Y_{fp}", bins, -15., 10.);
+   TH1F *hxpfp = new TH1F("hxpfp", hadron_arm + " X'_{fp}", bins, -0.06, 0.06 );
+   TH1F *hypfp = new TH1F("hypfp", hadron_arm + " Y'_{fp}", bins, -0.03, 0.02);
 
       
    //Electron Arm Reconstructed Quantities ( xtar, ytar, xptar, yptar, delta)
-   TH1F *eytar = new TH1F("eytar", "HMS Y_{tar}", bins, -4., 4.);
-   TH1F *exptar = new TH1F("exptar", "HMS X'_{tar}", bins, -0.08, 0.08);
-   TH1F *eyptar = new TH1F("eyptar", "HMS Y'_{tar}", bins, -0.04, 0.04);
-   TH1F *edelta = new TH1F("edelta", "HMS Momentum Acceptance, #delta", bins, -15., 15. );
+   TH1F *eytar = new TH1F("eytar", electron_arm + " Y_{tar}", bins, -4., 4.);
+   TH1F *exptar = new TH1F("exptar", electron_arm + " X'_{tar}", bins, -0.08, 0.08);
+   TH1F *eyptar = new TH1F("eyptar", electron_arm + " Y'_{tar}", bins, -0.04, 0.04);
+   TH1F *edelta = new TH1F("edelta", electron_arm + " Momentum Acceptance, #delta", bins, -15., 15. );
 
    //Electron Arm Focal Plane Quantities
-   TH1F *exfp = new TH1F("exfp", "HMS X_{fp}", bins, -60., 30.);
-   TH1F *eyfp = new TH1F("eyfp", "HMS Y_{fp}", bins, -20., 30.);
-   TH1F *expfp = new TH1F("expfp", "HMS X'_{fp}", bins, -0.1, 0.06);
-   TH1F *eypfp = new TH1F("eypfp", "HMS Y'_{fp}", bins, -0.04, 0.04);
+   TH1F *exfp = new TH1F("exfp", electron_arm + " X_{fp}", bins, -60., 30.);
+   TH1F *eyfp = new TH1F("eyfp", electron_arm + " Y_{fp}", bins, -20., 30.);
+   TH1F *expfp = new TH1F("expfp", electron_arm + " X'_{fp}", bins, -0.1, 0.06);
+   TH1F *eypfp = new TH1F("eypfp", electron_arm + " Y'_{fp}", bins, -0.04, 0.04);
 
 
 
@@ -169,6 +180,8 @@ void heep::Loop()
 
       //ANALYSIS OF EVENT-BY-EVENT GOES HERE!!!!!!
 
+
+      //Kinematics
       Emiss->Fill(Em, FullWeight);
       pm->Fill(Pm, FullWeight);
       Q_2->Fill(Q2, FullWeight);
@@ -177,31 +190,31 @@ void heep::Loop()
       theta_elec->Fill(theta_e, FullWeight);
       theta_prot->Fill(theta_p, FullWeight);
       
-
+      //Reconstructed Target Quantities (Lab Frame)
       x_tar->Fill(tar_x, FullWeight);
       y_tar->Fill(tar_y, FullWeight);
       z_tar->Fill(tar_z, FullWeight);
 
       
-      //HMS Quantities
+      //Hadron-Arm Target Reconstruction 
       hytar->Fill(h_ytar, FullWeight);
       hxptar->Fill(h_xptar, FullWeight);
       hyptar->Fill(h_yptar, FullWeight);
       hdelta->Fill(h_delta, FullWeight);
       
-      
+      //Hadron-Arm Focal Plane
       hxfp->Fill(h_xfp, FullWeight);
       hyfp->Fill(h_yfp, FullWeight);
       hxpfp->Fill(h_xpfp, FullWeight);
       hypfp->Fill(h_ypfp, FullWeight);
       
-      //SHMS Quantities
+      //Electron-Arm Target Reconstruction
       eytar->Fill(e_ytar, FullWeight);
       exptar->Fill(e_xptar, FullWeight);
       eyptar->Fill(e_yptar, FullWeight);
       edelta->Fill(e_delta, FullWeight);
       
-      
+      //Electron-Arm Focal Plane
       exfp->Fill(e_xfp, FullWeight);
       eyfp->Fill(e_yfp, FullWeight);
       expfp->Fill(e_xpfp, FullWeight);
