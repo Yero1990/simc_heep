a8127a87cc52ee9f0616bc7060ba37ee49fadee9
Carlos Yero <cyero002@fiu.edu>
2018-03-04 21:33:34 -0500

    added utilities directory for e12-10-003 experiment

---
+++ UTIL_ED/Macros/get_heep_report.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+runNo = sys.argv[1]
+evenNo = sys.argv[2]
+
+#Create reduced report output file
+out_filename = './UTIL_ED/REPORT_FILES/heep_report_%s.report'% (runNo)
+
+#Read REPORT File Name
+report_filename = './REPORT_OUTPUT/COIN/PRODUCTION/replay_coin_production_%s_%s.report'% (runNo, evenNo)
+
+line_no=0
+
+f = open(report_filename)
+fout = open(out_filename,'w')
+
+
+for line in f:
+    data = line.split(':')
+    if ('Run #' in data[0])  or ('HMS BCM4a Beam Cut Charge'in data[0]) or ('HMS BCM4b Beam Cut Charge'in data[0]) or('ROC2 Pre-Scaled Ps1 ROC2 Computer Live Time' in data[0]) or('E SING FID TRACK EFFIC'in data[0]or('HADRON SING FID TRACK EFFIC'in data[0])):#and
+    
+        line_no = line_no+1
+        if line_no == : ' '
+        
+        elif line_no == 7:
+            fout.write('HMS ' + data[0]+ ': '+data[1])
+
+        elif line_no == 8:
+            fout.write('HMS ' + data[0]+ ': '+ data[1])
+
+        elif line_no == 9:
+            fout.write('SHMS ' + data[0]+ ': '+ data[1])
+
+        elif line_no == 10:
+            fout.write('SHMS ' + data[0]+ ': '+ data[1])
+        else: 
+            fout.write(data[0]+ ': '+ data[1])
+        
+fout.close()
+f.close()
+
+
+
+#reopen output file to rename some variables
