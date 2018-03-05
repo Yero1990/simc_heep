4263e8580f41232c3e40b1bbfc3d52cc04becb26
Carlos Yero <cyero002@fiu.edu>
2018-03-04 21:33:34 -0500

    added utilities directory for e12-10-003 experiment

---
+++ UTIL_ED/Macros/Constants.h
@@ -0,0 +1,24 @@
+// Filename: Constants.h
+// Description: 
+// Author: Latif Kabir < latif@jlab.org >
+// Created: Wed Jan  3 20:46:53 2018 (-0500)
+// URL: jlab.org/~latif
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+//-- For E12-10-003 work directory --
+//#define ROOT_FILE_PATH "/work/hallc/e1210003/hallc_replay/ROOTfiles/"
+
+//#define ROOT_FILE_PATH "/work/hallc/e1210003/hallc_replay/ROOTfiles/"
+// -- on cdaql1 ---
+// #define ROOT_FILE_PATH "/net/cdaq/cdaql3data/cdaq/hallc-online/ROOTfiles/"
+//#define ROOT_FILE_PATH "/home/cdaq/hallc-online/hallc_replay/ROOTfiles/"
+
+//#define FILE_PREFIX "ep_"
+#define FILE_PREFIX "coin_replay_production_"
+#define FILE_SUFFIX "_-1"
+
+#define SOURCE_PATH "src/"
+
+#endif
